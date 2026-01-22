b672e4b201c3ab616883793fda065439057d8d44
牧山純 <jun.makiyama@rakus-partners.co.jp>
2026-01-22 09:51:10 +0900

    オブジェクト指向応用

--- 2.test-object-oriented/src/Student.java
+++ 2.test-object-oriented/src/Student.java
@@ -1,28 +1,33 @@
 public class Student {
+
     int energy;
-    public Student(){
+    Student(){
         this.energy = 50;
     }
 
     void sleep(){
-        this.energy += 50;
+        this.energy = energy + 50;
+
         if (this.energy >= 100) {
             this.energy = 100;
         }
         System.out.println("寝ました。エネルギーが"+energy+"になりました。");
-    if (this.energy == 100) {
+        if (this.energy == 100) {
             System.out.println("フルパワー！");
         }
     }
     
 
     void study(){
-        this.energy -= 30;
+        this.energy = energy - 30;
+
         if (this.energy <= 0) {
             this.energy = 0;
         }
+
         System.out.println("勉強しました。エネルギーが"+energy+"になりました。");
-    if (this.energy == 0) {
+        
+        if (this.energy == 0) {
             System.out.println("寝てください。");
         }
     }
