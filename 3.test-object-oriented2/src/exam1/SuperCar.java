f1c01abc3af88c83b599836767bae33dc7b91c4d
牧山純 <jun.makiyama@rakus-partners.co.jp>
2026-01-22 10:15:17 +0900

    オブジェクト指向応用

---
+++ 3.test-object-oriented2/src/exam1/SuperCar.java
@@ -0,0 +1,9 @@
+package exam1;
+
+public class SuperCar extends Car{
+
+    @Override
+    public void run(){
+        System.out.println("ブオーン!"+name+"が走ります");
+    }
+}
---
+++ 3.test-object-oriented2/src/exam3/DeLorean.java
@@ -0,0 +1,19 @@
+package exam3;
+
+public class DeLorean implements Car,Airplane,TimeMachine{
+
+        public void run(){
+            System.out.println("デロリアンが走る！");
+        }
+        public void fly(){
+            System.out.println("デロリアンが飛ぶ！");
+        }
+        public void timeTravel(){
+            System.out.println("デロリアンがタイムテレポート！");
+        }
+        public void engineStart(){
+            run();
+            fly();
+            timeTravel();
+        }
+}
